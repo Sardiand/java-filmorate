00f3b79d8369a03d32b3b190714dfe2e785af7fe
saryc <dsarychev92@yandex.ru>
2023-04-27 16:26:42 +0300

    fix: fix model style

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -9,12 +9,16 @@ import java.time.LocalDate;
 @Data
 public class Film {
     private Long id;
+
     @NotBlank(message = "Field \"name\" cannot be empty.")
     private String name;
+
     @Size(max = 200, message = "Field \"description\" cannot contain more than 200 symbols.")
     private String description;
+
     @AfterFirstFilmReleaseDate
     private LocalDate releaseDate;
+
     @NotNull(message = "Field \"duration\" cannot be empty.")
     @Positive(message = "Value of field \"duration\"must be greater than zero.")
     private Integer duration;
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -8,13 +8,17 @@ import java.time.LocalDate;
 @Data
 public class User {
     private Long id;
+
     @NotBlank(message = "Field \"Email\" cannot be empty.")
     @Email(message = "Invalid email format.")
     private String email;
+
     @NotBlank(message = "Field \"login\" cannot be empty.")
     @Pattern(regexp = "\\S+", message = "Field \"login\" cannot contain whitespaces.")
     private String login;
+
     private String name;
+
     @PastOrPresent(message = "Value of field \"birthday\" cannot be in the future.")
     private LocalDate birthday;
 }
